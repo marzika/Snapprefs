98c36b483a0c8f00abf165a15a1f15d2a9905a04
MARZ <marcellka23@hotmail.com>
2015-10-17 04:32:20 +0200

    *Fixed for 9.17.0.0

--- app/src/main/java/com/marz/snapprefs/Premium.java
+++ app/src/main/java/com/marz/snapprefs/Premium.java
@@ -11,7 +11,7 @@ import static de.robv.android.xposed.XposedHelpers.findClass;
 
 public class Premium {
     static void initReplay(final XC_LoadPackage.LoadPackageParam lpparam, final XModuleResources modRes, final Context snapContext) {
-        findAndHookMethod("auz", lpparam.classLoader, "d", XC_MethodReplacement.returnConstant(true));
+        findAndHookMethod(Obfuscator.save.USER_CLASS, lpparam.classLoader, "d", XC_MethodReplacement.returnConstant(true));
         findAndHookMethod(Obfuscator.save.RECEIVEDSNAP_CLASS, lpparam.classLoader, "y", XC_MethodReplacement.returnConstant(false));
         findAndHookMethod(Obfuscator.save.RECEIVEDSNAP_CLASS, lpparam.classLoader, "u", XC_MethodReplacement.returnConstant(false));
         //findAndHookMethod("aty", lpparam.classLoader, "b",XC_MethodReplacement.DO_NOTHING);
@@ -29,6 +29,6 @@ public class Premium {
 
     static void initViewed(final XC_LoadPackage.LoadPackageParam lpparam, final XModuleResources modRes, final Context snapContext) {
         Class<?> receivedsnap = findClass(Obfuscator.save.RECEIVEDSNAP_CLASS, lpparam.classLoader);
-        findAndHookMethod(Obfuscator.save.USER_CLASS, lpparam.classLoader, "a", receivedsnap, XC_MethodReplacement.DO_NOTHING);
+        findAndHookMethod(Obfuscator.save.USER_CLASS, lpparam.classLoader, "a", receivedsnap, boolean.class, XC_MethodReplacement.DO_NOTHING);
     }
 }
