f365c32a03995eecc008c67cc1c71770e8d818cc
MARZ <maaaaarz@gmail.com>
2016-04-26 18:58:25 +0200

    *Fixed name error by Git

--- app/src/main/java/com/marz/snapprefs/GcmBroadcastReceiver.java
+++ app/src/main/java/com/marz/snapprefs/GcmBroadcastReceiver.java
@@ -11,7 +11,6 @@ import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
 
 public class GCMBroadcastReceiver extends WakefulBroadcastReceiver {
-
     @Override
     public void onReceive(Context context, Intent intent) {
         // Attach component of GCMIntentService that will handle the intent in background thread
