100e731710c8d367cddb1336233ec43f1989fa31
Piotr Brzozowski <stirante@interia.eu>
2016-08-22 20:31:09 +0200

    Another null check

--- app/src/main/java/com/marz/snapprefs/HookMethods.java
+++ app/src/main/java/com/marz/snapprefs/HookMethods.java
@@ -485,10 +485,12 @@ public class HookMethods implements IXposedHookInitPackageResources, IXposedHook
                 findAndHookMethod(Obfuscator.icons.ICON_HANDLER_CLASS, lpparam.classLoader, Obfuscator.icons.SHOW_LENS, boolean.class, boolean.class, new XC_MethodHook() {
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        if((boolean)param.args[0]){
-                            HookedLayouts.upload.setVisibility(View.INVISIBLE);
-                        } else {
-                            HookedLayouts.upload.setVisibility(View.VISIBLE);
+                        if (HookedLayouts.upload != null) {
+                            if((boolean)param.args[0]){
+                                HookedLayouts.upload.setVisibility(View.INVISIBLE);
+                            } else {
+                                HookedLayouts.upload.setVisibility(View.VISIBLE);
+                            }
                         }
                     }
                 });
