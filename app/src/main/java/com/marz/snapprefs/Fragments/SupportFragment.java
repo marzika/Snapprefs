5234c671214c5c216f8abe1ab9944a5bba82fadf
marzika <maaaaarz@gmail.com>
2016-11-18 00:56:45 +0100

    Added Facebook, Website and Documentation to the Support

--- app/src/main/java/com/marz/snapprefs/Fragments/SupportFragment.java
+++ app/src/main/java/com/marz/snapprefs/Fragments/SupportFragment.java
@@ -23,13 +23,62 @@ public class SupportFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.support_layout,
                 container, false);
+
+        ImageButton facebook = (ImageButton) view.findViewById(R.id.facebook);
+        ImageButton website = (ImageButton) view.findViewById(R.id.website);
         ImageButton xda = (ImageButton) view.findViewById(R.id.xda);
+        ImageButton documentation = (ImageButton) view.findViewById(R.id.docs);
         ImageButton googlePlusBtn  = (ImageButton) view.findViewById(R.id.gplus);
         ImageButton reddit = (ImageButton) view.findViewById(R.id.reddit);
+
+        facebook.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/snapprefs/"));
+                    startActivity(myIntent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(getActivity().getApplicationContext(), "No application can handle this request." + " Please install a web browser", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+        website.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.snapprefs.com"));
+                    startActivity(myIntent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(getActivity().getApplicationContext(), "No application can handle this request." + " Please install a web browser", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
         xda.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 try {
                     Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://forum.xda-developers.com/xposed/modules/app-snapprefs-ultimate-snapchat-utility-t2947254/"));
+                    startActivity(myIntent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(getActivity().getApplicationContext(), "No application can handle this request." + " Please install a web browser", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+        documentation.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://snapprefs.readthedocs.io"));
+                    startActivity(myIntent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(getActivity().getApplicationContext(), "No application can handle this request." + " Please install a web browser", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+        reddit.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.reddit.com/r/snapprefs"));
                     startActivity(myIntent);
                 } catch (ActivityNotFoundException e) {
                     Toast.makeText(getActivity().getApplicationContext(), "No application can handle this request." + " Please install a web browser", Toast.LENGTH_LONG).show();
@@ -44,17 +93,6 @@ public class SupportFragment extends Fragment {
                     startActivity(myIntent);
                 } catch (ActivityNotFoundException e) {
                     Toast.makeText(getActivity().getApplicationContext(), "No application can handle this request." + " Please install a webbrowser", Toast.LENGTH_LONG).show();
-                    e.printStackTrace();
-                }
-            }
-        });
-        reddit.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                try {
-                    Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.reddit.com/r/snapprefs"));
-                    startActivity(myIntent);
-                } catch (ActivityNotFoundException e) {
-                    Toast.makeText(getActivity().getApplicationContext(), "No application can handle this request." + " Please install a web browser", Toast.LENGTH_LONG).show();
                     e.printStackTrace();
                 }
             }
